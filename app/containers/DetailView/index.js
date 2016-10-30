93247d52e8d67d38937913a93d342678e5cb8953
Tengis Batsaikhan <tengee0411@gmail.com>
2016-10-31 08:01:22 +1100

    fix

--- app/containers/DetailView/index.js
+++ app/containers/DetailView/index.js
@@ -100,9 +100,9 @@ export class DetailView extends React.Component { // eslint-disable-line react/p
 
               <div className="col-sm-5">
                 {entryTitle}
-                <div className={styles.price}>${entry.price.toLocaleString()}</div>
-                {entry.amount ? <div><b>+${entry.amount.toLocaleString()} security deposit</b></div> : ''}
-                {entry.squareFeet ? <div>{entry.squareFeet.toLocaleString()} square feet</div> : ''}
+                <div className={styles.price}>${entry.price}</div>
+                {entry.amount ? <div><b>+${entry.amount} security deposit</b></div> : ''}
+                {entry.squareFeet ? <div>{entry.squareFeet} square feet</div> : ''}
                 <Map
                   containerElement={
                     <div style={{ height: 300 }} />

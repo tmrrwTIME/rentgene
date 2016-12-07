a58a4d086bfb50c5148070e5b19ab5e1e8740c1c
Carlos Gonzalez <cyberpolin@gmail.com>
2016-12-07 15:52:12 -0600

    Add gallery done

--- app/components/Gallery/index.js
+++ app/components/Gallery/index.js
@@ -24,12 +24,11 @@ class Gallery extends Component{
       images.map((item, i)=>{
         return(
           <Carousel.Item key={i}>
-            <Link to={this.props.to}>
+            <Link to={this.props.to} className='ScaledImg'>
               <img
                 className={`${id}-slides`}
                 src={`https://s3-us-west-2.amazonaws.com/rentgene-uploads/images/${item.name}`}
                 role="presentation"
-                style={{height:250, margin:'0 auto'}}
               />
             </Link>
           </Carousel.Item>
@@ -40,11 +39,34 @@ class Gallery extends Component{
   changeSize(e){
     console.log(e);
     var element = document.getElementsByClassName('carousel-inner')[0]
-    var width = window.getComputedStyle(element).height;
+    var height = window.getComputedStyle(element).height;
+    var width = window.getComputedStyle(element).width;
+
     console.log(width);
   }
   componentWillReceiveProps(nextProps){
     this.setState({images: nextProps.images})
+
+  }
+  componentDidMount(){
+    var element = document.getElementsByClassName('carousel-inner')[0]
+    var image = document.getElementsByClassName('carousel-images')
+    var height = window.getComputedStyle(element).height;
+    var width = window.getComputedStyle(element).width;
+
+
+
+    console.log(image);
+    console.log(this.state.images);
+    for (var i = 0; i < image.length; i++) {
+      console.log(image[i]);
+
+    }
+    // images.map((image)=>{
+    //   console.log(image);
+    // })
+    console.log('heigth', height);
+    console.log('width', width);
 
   }
   componentWillUpdate(){
